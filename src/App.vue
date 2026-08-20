69f6167517ba7d75daf3c283b7ac0a14b4ac2da5
ABB702 <3294527206@qq.com>
2026-08-20 11:20:42 +0800

    b2

--- src/App.vue
+++ src/App.vue
@@ -55,7 +55,7 @@ const init = async () => {
 }
 
 const changeActive = index => {
-  console.log('feature-log-b1', index)
+  console.log('feature-log-b2', index)
   state.activePage = index
 }
 </script>>
